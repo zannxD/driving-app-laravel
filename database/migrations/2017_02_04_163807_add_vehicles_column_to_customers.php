87a9f9d3de5a04961bbef10443eff9fa9f50476f
Dimuthu de Silva <dimuthu.d@eblix.com.au>
2017-02-04 22:10:44 +0530

    changes

---
+++ database/migrations/2017_02_04_163807_add_vehicles_column_to_customers.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddVehiclesColumnToCustomers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('customers', function (Blueprint $table) {
+            $table->string('vehicles')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('customers', function (Blueprint $table) {
+            //
+        });
+    }
+}
